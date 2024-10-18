f999ae485815137dc7f312f749ad73fd5128dbb1
student17 <student17@teb.pl>
2024-10-18 14:13:09 +0200

    commit 2

--- main.py
+++ main.py
@@ -8,7 +8,31 @@ class MyForm(QWidget):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+
         self.show()
+
+    def rozmiar_click(self):
+        rozmiar = ""
+        dostawa = ""
+
+        if self.ui.radio_s.isChecked():
+            rozmiarc = "S"
+        elif self.ui.radio_m.isChecked():
+            rozmiar = "M"
+        elif self.ui.radio_l.isChecked():
+            rozmiar = "L"
+        elif self.ui.radio_xl.isChecked():
+            rozmiar = "XL"
+
+        if self.ui.radio_paczkomat.isChecked():
+            dostawa = "Paczkomat"
+        elif self.ui.radio_kurier.isChecked():
+            dostawa = "Kurier"
+        elif self.ui.radio_odbior.isChecked():
+            dostawa = "Odbiór osobisty"
+
+        self.ui.wynik_label.setText(f'Rozmiar twojej koszulki to: {rozmiar} i sposób dostawy: ')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
